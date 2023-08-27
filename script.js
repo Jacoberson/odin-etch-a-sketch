576efa22551f58a855f3269d113f05dbae45bea0
Jacoberson <c.jacob.moore92@gmail.com>
2023-08-27 15:20:20 -0500

    Fix for loop to have correct number of squares

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const container = document.querySelector('#container');
 
 const divArr = [];
 
-for (let i = 1; i <= 256; i++) {
+for (let i = 1; i < 256; i++) {
     divArr[i] = document.createElement('div');
     divArr[i].setAttribute('id', `pixel ${i}`);
     divArr[i].setAttribute('class', 'pixel');
