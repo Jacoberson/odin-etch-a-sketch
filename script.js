ebedfa5cc2490f909098e5dc2b5bb462879ef543
Jacoberson <c.jacob.moore92@gmail.com>
2023-08-27 15:15:01 -0500

    Add EventListener for pixels
    - Removed background color change from CSS
    - Instead added event listener for color change

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ const divArr = [];
 
 for (let i = 1; i <= 256; i++) {
     divArr[i] = document.createElement('div');
-    divArr[i].setAttribute('id', `div ${i}`);
-    divArr[i].setAttribute('class', 'inner-div');
+    divArr[i].setAttribute('id', `pixel ${i}`);
+    divArr[i].setAttribute('class', 'pixel');
     container.appendChild(divArr[i]);
 }
+
+const pixels = document.querySelectorAll('.pixel');
+
+pixels.forEach(pixel => {
+    pixel.addEventListener('mouseover', event => {
+        event.target.style.backgroundColor = 'yellow';
+    })
+})
