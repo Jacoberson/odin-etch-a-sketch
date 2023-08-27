72a37a4ed99de56906dfaae52e118815c92deae5
Jacoberson <c.jacob.moore92@gmail.com>
2023-08-27 15:11:19 -0500

    Add color change on hover

--- script.js
+++ script.js
@@ -6,6 +6,5 @@ for (let i = 1; i <= 256; i++) {
     divArr[i] = document.createElement('div');
     divArr[i].setAttribute('id', `div ${i}`);
     divArr[i].setAttribute('class', 'inner-div');
-    divArr[i].textContent = `${i}`
     container.appendChild(divArr[i]);
 }
