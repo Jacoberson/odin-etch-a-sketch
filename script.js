5e34d517bf5c47aa507a959df0728bd96a0287df
Jacoberson <c.jacob.moore92@gmail.com>
2023-08-27 18:04:54 -0500

    Add method to draw grid based on number

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('#container');
 const button = document.querySelector('button');
+const divArr = [];
 
 button.addEventListener('click', () => {
     chooseNumberOfSquares();
@@ -8,28 +9,31 @@ button.addEventListener('click', () => {
 const chooseNumberOfSquares = () => {
     const numberOfSquares = prompt('Choose a number between 1 and 100 for squares per side of the grid');
 
-    return numberOfSquares > 100 ? alert('Choose a number between 1 and 100') : removeGrid();
+    return numberOfSquares > 100 ? alert('Choose a number between 1 and 100') : removeAndDrawGrid(numberOfSquares);
 }
 
-const removeGrid = () => {
+const removeAndDrawGrid = numberOfSquares => {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
+
+    for (let i = 1; i <= numberOfSquares ** 2; i++) {
+        divArr[i] = document.createElement('div');
+        divArr[i].setAttribute('id', `pixel-${i}`);
+        divArr[i].setAttribute('class', 'pixel');
+        container.appendChild(divArr[i]);
+    }
+
+    const pixels = document.querySelectorAll('.pixel');
+
+    pixels.forEach(pixel => {
+        pixel.addEventListener('mouseover', event => {
+            event.target.style.backgroundColor = 'yellow';
+        })
+    });
 }
 
-const divArr = [];
 
-for (let i = 1; i < 256; i++) {
-    divArr[i] = document.createElement('div');
-    divArr[i].setAttribute('id', `pixel ${i}`);
-    divArr[i].setAttribute('class', 'pixel');
-    container.appendChild(divArr[i]);
-}
 
-const pixels = document.querySelectorAll('.pixel');
 
-pixels.forEach(pixel => {
-    pixel.addEventListener('mouseover', event => {
-        event.target.style.backgroundColor = 'yellow';
-    })
-})
+
